a7a1c68ac75471a9b9d91362f14c3c2f0a3a12e0
linus22765566 <linus22765566@gmail.com>
2022-10-23 06:23:42 +0800

    final version of backend

--- server.py
+++ server.py
@@ -1,10 +1,11 @@
 from tkinter import E
-from flask import Flask,request
+from flask import Flask,request, jsonify
 from flask_cors import CORS
 import cv2
 import io
 import base64
 import numpy as np
+import image
 app = Flask(__name__)
 CORS(app)
 
@@ -25,7 +26,7 @@ def pack_image(img):
     img_encoded = cv2.imencode('.png',img)
     base64_str = img_encoded[1].tobytes()
     img_encoded = base64.b64encode(base64_str)
-    return img_encoded
+    return img_encoded.decode('ASCII')
     
 
 @app.route("/uploadImage/", methods=['GET', 'POST'])
@@ -37,14 +38,22 @@ def test():
         origin_image = parse_image(origin_image,'origin.png')
         golden_image = parse_image(golden_image,'golden.png')
         
-        processed_image = pack_image(origin_image)
+        # ---> type: dict[7] ndarray: uint8
+        all_images = image.process_images(origin_image,golden_image)
+        final = []
+        result = {}
+        for key in all_images.keys():
+            cv2.imwrite(f'{key}.png',all_images[key])
+            result[key] = pack_image(all_images[key])
         
-        
-        return processed_image
+        result_json = jsonify(result)
+        # print("result json:", result, result_json)
+        return result_json
+    
     except Exception as e:
         print(e)
         return "error"
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
